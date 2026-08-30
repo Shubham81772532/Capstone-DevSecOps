45a5cfdcc858c3b74262e1cd518a64f0354828de
Shubham81772532 <shubhamnavale712@gmail.com>
2026-08-30 20:23:40 +0530

    test automated cd

--- Terraform/modules/EKS/eksmain.tf
+++ Terraform/modules/EKS/eksmain.tf
@@ -11,6 +11,22 @@ module "eks" {
   endpoint_public_access  = true
   endpoint_private_access = false
 
+  authentication_mode = "API_AND_CONFIG_MAP"
+
+  addons = {
+    vpc-cni = {
+      most_recent = true
+    }
+
+    kube-proxy = {
+      most_recent = true
+    }
+
+    coredns = {
+      most_recent = true
+    }
+  }
+
   eks_managed_node_groups = {
     hotstar_nodes = {
       instance_types = var.eks_instance_types
--- Terraform/modules/VPC/vpcmain.tf
+++ Terraform/modules/VPC/vpcmain.tf
@@ -115,7 +115,7 @@ resource "aws_route_table" "private_rt" {
 # Public Route Table Association
 # -----------------------------
 resource "aws_route_table_association" "public_assoc" {
-  count          = length(aws_subnet.public_subnet)
+  count = length(aws_subnet.public_subnet)
 
   subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public_rt.id
@@ -127,7 +127,7 @@ resource "aws_route_table_association" "public_assoc" {
 # =====================================================
 
 resource "aws_route_table_association" "private_assoc" {
-  count          = length(aws_subnet.private_subnet)
+  count = length(aws_subnet.private_subnet)
 
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private_rt.id
